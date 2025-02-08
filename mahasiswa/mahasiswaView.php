a873f16e75207de89a9dd3ba5bb169eb4c575469
Ramdani <110803481+Ramdanii19@users.noreply.github.com>
2025-02-08 11:26:01 +0700

    feat: view mahasiswa

---
+++ mahasiswa/mahasiswaView.php
@@ -0,0 +1,94 @@
+<?php
+include "../koneksi/koneksi.php";
+
+$queryMhs = "SELECT * FROM mahasiswa";
+$resultMhs = mysqli_query($koneksi, $queryMhs);
+$countMhs = mysqli_num_rows($resultMhs);
+?>
+
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+  <title>.: Sistem Informasi Nilai Online - Universitas Komputer Indonesia :.</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <link rel="shortcut icon" type="image/x-icon" href="../images/logoicon.ico" />
+  <link href='http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold' rel='stylesheet' type='text/css' />
+  <link href='http://fonts.googleapis.com/css?family=Kreon:light,regular' rel='stylesheet' type='text/css' />
+  <link rel="stylesheet" type="text/css" href="../css/style-sheet.css" />
+  <link rel="stylesheet" type="text/css" href="../css/nivo-slider.css" />
+</head>
+
+<body onLoad="initialize()">
+  <header>
+    <section class="logo"><a href="#"><img src="../images/logo.png" /></a></section>
+    <section class="title">Sistem Informasi Nilai Online <br /> <span>POLITEKNIK POS BANDUNG</span></section>
+    <section class="clr">
+      <center>Jl.Sariasih No.54, Sarijadi, Sukasari, Kota Bandung, Jawa Barat 40151</center>
+    </section>
+  </header>
+
+  <section id="navigator">
+    <ul class="menus">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="../mahasiswa/mahasiswaView.php">Mahasiswa</a></li>
+      <li><a href="../dosen/dosenView.php">Dosen</a></li>
+      <li><a href="../mahasiswa/nilaiView.php">Nilai</a></li>
+      <li><a href="../index.php">Logout</a></li>
+    </ul>
+  </section>
+
+  <section id="container">
+    <br><br><br>
+    <div style="padding: 0 30px 0 30px;">
+      <div style="display: flex; justify-content: end;">
+        <a href="mahasiswaAdd.php" style="background-color: green; padding: 10px 20px 10px 20px; border-radius: 7px; font-weight: bold;">Tambah Data Mahasiswa</a>
+      </div>
+      <br><br>
+
+      <table style="width: 100%; color: white; border-collapse: collapse; border: white;" border="1">
+        <tr>
+          <th>NIM</th>
+          <th>Nama</th>
+          <th>Jenis Kelamin</th>
+          <th>Jurusan</th>
+          <th>Password</th>
+          <th>Aksi</th>
+        </tr>
+        <?php
+
+        if ($countMhs > 0) {
+          while ($dataMhs = mysqli_fetch_array($resultMhs, MYSQLI_ASSOC)) {
+            echo "<td>" . $dataMhs['nim'] . "</td>";
+            echo "<td>" . $dataMhs['nama'] . "</td>";
+            echo "<td>" . $dataMhs['jk'] . "</td>";
+            echo "<td>" . $dataMhs['jur'] . "</td>";
+            echo "<td>" . $dataMhs['password'] . "</td>";
+            echo "<td>
+              <a href='mahasiswaEdit.php?nim=" . $dataMhs['nim'] . "'>Edit</a> |
+              <a href='mahasiswaDelete.php?nim=" . $dataMhs['nim'] . "' onclick='return confirm(\"Yakin ingin menghapus?\")'>Delete</a>
+            </td>";
+            echo "</tr>";
+          }
+        } else {
+          echo "<tr>
+                <td colspan='9' align='center' height='20'>
+                  <div> Belum ada data Mahasiswa </div>
+                </td>
+            </tr>";
+        }
+        ?>
+      </table>
+      <br><br><br><br><br><br>
+    </div>
+    <section class="clr"></section>
+  </section>
+
+  <footer>
+    <font color=#000> Copyright &copy; 2023 - Universitas Komputer Indonesia <br />
+      Developed By <a href="#" target="_new">YourName</a> </font>
+  </footer>
+
+</body>
+
+</html>
